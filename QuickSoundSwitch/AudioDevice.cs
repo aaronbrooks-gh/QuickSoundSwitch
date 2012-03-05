6e996892a4353a7033a757a06f82f1bda3cbb2e8
Aaron Brooks <brooksaar@gmail.com>
2012-03-05 21:44:46 +1100

    Rewrote QSS to take advantage of interop between AudioEndPointController.dll and our C#.
    Signed-off-by: Aaron Brooks <brooksaar@gmail.com>

---
+++ QuickSoundSwitch/AudioDevice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices; //allows our interop code
+
+namespace QuickSoundSwitch
+{
+    class AudioDevice
+    {
+        private int deviceNum;
+        private UnmanagedType deviceID;
+        private string deviceName;
+
+        public int DeviceNum
+        {
+            get
+            {
+                return deviceNum;
+            }
+        }
+        public string DeviceName
+        {
+            get
+            {
+                return deviceName;
+            }
+        }
+
+        public AudioDevice(int deviceNum)
+        {
+            this.deviceNum = deviceNum;
+            this.deviceID = GetDeviceID(this.deviceNum);
+            this.deviceName = GetDeviceFriendlyName(this.deviceNum);
+        }
+
+        public void SetDefault()
+        {
+            SetDefaultAudioPlaybackDevice(deviceID);
+        }
+
+        [DllImport("AudioEndPointController.dll", EntryPoint = "?SetDefaultAudioPlaybackDevice@@YAJPB_W@Z", CallingConvention = CallingConvention.Cdecl)]
+        static extern Int32 SetDefaultAudioPlaybackDevice(UnmanagedType deviceID);
+
+        [DllImport("AudioEndPointController.dll", EntryPoint = "?GetDeviceFriendlyName@@YAPA_WH@Z", CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.LPWStr)]
+        static extern string GetDeviceFriendlyName(int deviceNum);
+
+        [DllImport("AudioEndPointController.dll", EntryPoint = "?GetDeviceID@@YAPB_WH@Z", CallingConvention = CallingConvention.Cdecl)]
+        static extern UnmanagedType GetDeviceID(int deviceNum);
+    }
+}
--- QuickSoundSwitch/Program.cs
+++ QuickSoundSwitch/Program.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Runtime.InteropServices; //allows our interop code
 using NAudio;
 using NAudio.CoreAudioApi;
 
@@ -16,7 +17,6 @@ namespace QuickSoundSwitch
             Text = "QuickSoundSwitch"
         };
         static ContextMenu trayMenu = new ContextMenu();
-        static Process endPointController = new Process();
 
         static void Main()
         {
@@ -24,69 +24,45 @@ namespace QuickSoundSwitch
             Application.Run();
         }
 
-        static private string[,] GetDevices()
+        static AudioDevice[] GetDevices()
         {
-            string[,] deviceList; //DeviceNumber,DeviceName
-            string tempString;
-            string[] tempArray;
+            AudioDevice[] deviceList = new AudioDevice[GetDeviceCount()];
 
-            endPointController.StartInfo = new ProcessStartInfo("Externals\\EndPointController.exe")
+            for (int i = 0; i < GetDeviceCount(); i++)
             {
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true
-            };
-
-            endPointController.Start();
-            tempString = endPointController.StandardOutput.ReadToEnd().ToString();
-            tempArray = new string[tempString.Split(new string[1] { "\n" }, StringSplitOptions.None).Length];
-            tempArray = tempString.Split(new string[1] { "\n" }, StringSplitOptions.None);
-
-            deviceList = new string[tempArray.Length - 1, 2]; //length -1 because we have an extra line that we don't need.
-
-            for (int i = 0; i < tempArray.Length - 1; i++)
-            {
-                deviceList[i, 0] = tempArray[i].Split(new string[2] { " ", ":" }, StringSplitOptions.None)[2];
-                deviceList[i, 1] = tempArray[i].Split(new string[1] { ":" }, StringSplitOptions.None)[1].Trim();
+                deviceList[i] = new AudioDevice(i);
             }
+
             return deviceList;
         }
 
-        static private void SetDevice(string deviceID)
-        {
-            endPointController.StartInfo = new ProcessStartInfo("Externals\\EndPointController.exe", deviceID)
-            {
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true
-            };
-
-            endPointController.Start();
-            
-        }
-
-        static private void CreateTray()
+        static void CreateTray()
         {
             trayIcon.ContextMenu = trayMenu;
-            trayMenu.Popup += new EventHandler(trayMenu_Popup);          
+            trayMenu.Popup += new EventHandler(trayMenu_Popup);      
         }
 
         static void trayMenu_Popup(object sender, EventArgs e)
         {
-            string[,] deviceList = GetDevices();
+            AudioDevice[] deviceList = GetDevices();
             MMDevice defaultDevice = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
 
             trayMenu.MenuItems.Clear();
 
             trayMenu.MenuItems.Add("Current: " + defaultDevice.FriendlyName + " (" + defaultDevice.DeviceFriendlyName + ")");
             trayMenu.MenuItems.Add("-");
-            for (int i = 0; i < deviceList.GetLength(0); i++)
+
+            for (int i = 0; i < deviceList.Length; i++)
             {
                 int localI = i; //We create a local int so that we don't have problems with the delegate inside the for loop.
-                trayMenu.MenuItems.Add("[" + deviceList[i, 0] + "] " + deviceList[i, 1], (f, g) => SetDevice(deviceList[localI, 0]));
+                trayMenu.MenuItems.Add(/*"[" + (deviceList[i].DeviceNum+1) + "] " + */deviceList[i].DeviceName, (f, g) => deviceList[localI].SetDefault());
             }
+
             trayMenu.MenuItems.Add("-");
             trayMenu.MenuItems.Add("Exit", (f, g) => Application.Exit());
         }
+
+        [DllImport("AudioEndPointController.dll", EntryPoint = "?GetDeviceCount@@YAHXZ", CallingConvention = CallingConvention.Cdecl)] //Get the entrypoint using dumpbin.exe /exports.
+        static extern int GetDeviceCount();
     }
 }
